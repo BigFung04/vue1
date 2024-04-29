10ca6f084056d41edf99b40c8653fd35923d76ce
BigFung04 <163716859+BigFung04@users.noreply.github.com>
2024-04-29 22:41:22 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter,createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import TodoList from '../components/todolist_view.vue'
 import Biography from '../components/Biography.vue'
+import Class_grades from '../components/Class_grades.vue'
+import person_grades from '../components/person_grades.vue'
+
+
+
 import { useAuthStore } from '../store';
 const router = createRouter({
     history: createWebHashHistory(),
@@ -23,7 +28,17 @@ const router = createRouter({
         name: 'Biography',
         component: Biography,
         meta: { requiresAuth: true }
-      }
+      },
+      {
+        path: '/Class_grades',
+        name: 'Class_grades',
+        component: Class_grades
+      },
+      {
+        path: '/person_grades',
+        name: 'person_grades',
+        component: person_grades
+      },
     ],
   })
 
