fa42c885b61716ed8d7daf3dd359f40a45047a60
BigFung04 <163716859+BigFung04@users.noreply.github.com>
2024-04-29 22:40:45 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app" class="app-background">
+  <div id="app" :class="backgroundClass">
     <router-link to="/" class="rt">Home</router-link>
     <router-link to="/todolist" class="rt">todoList</router-link>
     <router-link to="/Biography" class="rt">Biography</router-link>
+    <router-link to="/Class_grades" class="rt">Class_grades</router-link>
+    <router-link to="/person_grades" class="rt">person_grades</router-link>
   </div>
   <router-view></router-view>
   <Login v-if="!authStore.isAuthenticated" />
@@ -11,36 +13,56 @@
 <script>
 import { useAuthStore } from './store'; 
 import Login from './components/login.vue';
+import { ref, watchEffect } from 'vue';
 
 export default {
   name: "App",
+  components: {
+    Login,
+  },
   setup() {
     const authStore = useAuthStore(); 
+    const backgroundClass = ref('');
+
+    const checkScreenWidth = () => {
+      backgroundClass.value = window.innerWidth <= 768 ? 'app-background-mobile' : 'app-background-desktop';
+    };
+
+    watchEffect(() => {
+      window.addEventListener('resize', checkScreenWidth);
+      checkScreenWidth();
+      return () => window.removeEventListener('resize', checkScreenWidth);
+    });
+
     return {
-      authStore, 
+      authStore,
+      backgroundClass,
     };
   },
-  components: {
-    Login, 
-  },
-}
+};
 </script>
 
 <style scoped>
-.rt
-{
-  width: 20px;
-  margin-left: 20px;
-  color: #fff;
-}
-.app-background
-{
+.app-background {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   height: 60px;
   padding: 0 70px;
-  background: linear-gradient(to right, #ff7e5f, #feb47b);
   border-bottom: 2px solid #f38181;
+}
+
+.app-background-mobile {
+  background: linear-gradient(to right, #ff7e5f, #feb47b);
+}
+
+.app-background-desktop {
+  background: linear-gradient(to bottom, #ff7e5f, #feb47b);
+}
+
+.rt {
+  width: 20px;
+  margin-left: 20px;
+  color: #fff;
 }
 </style>
